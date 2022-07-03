a794d8bdbaa8c102e2a2aaec687cec49f970c63e
Alpredo Pandiangan <alpredo.pandiangan@rey.id>
2022-07-03 10:00:17 +0700

    Golang Dasar - Chapter A.19

--- chapter-A.19-fungsi-multiple-return/1-fungsi-multiple-return.go
+++ chapter-A.19-fungsi-multiple-return/1-fungsi-multiple-return.go
@@ -1,14 +1,15 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func calculate(d float64) (float64, float64) {
 	// hitung luas
 	var area = math.Pi * math.Pow(d/2, 2)
 	// hitung keliling
 	var circumference = math.Pi * d
-
 	// kembalikan 2 nilai
 	return area, circumference
 }
